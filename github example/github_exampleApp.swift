b0b6f9f983547199245c122fbff8fca0e1017832
Ben <benlevinmarketing@gmail.com>
2024-12-10 09:34:44 -0500

    Initial Commit

---
+++ github example/github_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_exampleApp.swift
+//  github example
+//
+//  Created by Ben Levin on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct github_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
